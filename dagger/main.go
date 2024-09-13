a52d27a18084913570db32799d55b792fc10830a
pandemicsyn <florian@replicated.com>
2024-09-12 22:37:33 -0500

    init

---
+++ dagger/main.go
@@ -0,0 +1,60 @@
+// A generated module for Fmysql functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"dagger/fmysql/internal/dagger"
+)
+
+type Fmysql struct{}
+
+func (m *Fmysql) fmysql(source *dagger.Directory) *dagger.Container {
+	return dag.Container().From("mysql:8.4").
+		WithFile("/docker-entrypoint-initdb.d/fixtures.sql", source.File("/fixtures.sql"), dagger.ContainerWithFileOpts{
+			Owner: "mysql:mysql",
+		}).
+		WithEnvVariable("MYSQL_USER", "fmysql").
+		WithEnvVariable("MYSQL_PASSWORD", "password").
+		WithEnvVariable("MYSQL_DATABASE", "fmysql").
+		WithEnvVariable("MYSQL_RANDOM_ROOT_PASSWORD", "1").
+		WithFile("/etc/mysql/conf.d/docker.cnf", source.File("docker.cnf")).
+		WithDefaultArgs([]string{"mysqld", "--mysql-native-password=FORCE"}).
+		WithExposedPort(3306)
+}
+
+func (m *Fmysql) Testit(source *dagger.Directory) (string, error) {
+	mysqlCtr := m.fmysql(source)
+	mysqlSvc := mysqlCtr.AsService()
+	ctx := context.Background()
+
+	return dag.Container().From("mysql:8.4").
+		WithServiceBinding("mysqlsvc", mysqlSvc).
+		WithExec([]string{
+			"mysql",
+			"-h", "mysqlsvc",
+			"-u", "fmysql",
+			"-ppassword", // Note: The -p and password are concatenated
+			"-e", "USE fmysql; SELECT @@sql_mode;",
+		}).
+		WithExec([]string{
+			"mysql",
+			"-h", "mysqlsvc",
+			"-u", "fmysql",
+			"-ppassword", // Note: The -p and password are concatenated
+			"-e", "USE fmysql; SELECT * FROM testable ORDER BY created_at DESC LIMIT 5;",
+		}).Stdout(ctx)
+
+}
---
+++ fixtures.sql
@@ -0,0 +1,7 @@
+-- Create the table
+CREATE TABLE IF NOT EXISTS testable (
+    created_at DATETIME
+);
+
+-- Insert a sample record
+INSERT INTO testable (created_at) VALUES ("2019-01-01T01:23:45.678Z");
