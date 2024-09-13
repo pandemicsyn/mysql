578a77191bb789a76d76badfb21ec35a7520fd9e
pandemicsyn <florian@replicated.com>
2024-09-12 22:46:57 -0500

    wtf

--- fixtures.sql
+++ fixtures.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS testable (
     created_at DATETIME
 );
 
--- Insert a sample record
+-- Insert a sample records
 --INSERT INTO testable (created_at) VALUES ("2019-01-01T01:23:45.678Z");
 INSERT INTO testable (created_at) VALUES (NOW());
